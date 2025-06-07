7108b90876ea6a80e59280c5508e7436d2020f4c
Miłosz Godyń <milosz@milgodyn.dev>
2025-06-07 19:56:14 +0200

    Fix logout

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.get('/login', (req, res) => {
-    res.render('login', { title: 'Login', error: null });
+    res.render('login', { title: 'Login', error: null, logoutMessage: null });
 });
 
 router.post('/login', async (req, res) => {
@@ -12,11 +12,20 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ username });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.render('login', { title: 'Login', error: 'Username or password is incorrect' });
+        return res.render('login', { title: 'Login', error: 'Username or password is incorrect', logoutMessage: null });
     }
 
     req.session.userId = user._id;
     res.redirect('/admin');
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error destroying session:', err);
+        }
+        res.render('login', { title: 'Login', error: null, logoutMessage: 'You have been logged out' });
+    });
 });
 
 module.exports = router;
